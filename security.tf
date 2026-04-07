1243c1cdd7049f13906f4579dd920e9f29aad0f5
Ishihara <almond1024r@gmail.com>
2026-04-08 00:58:34 +0900

    セキュリティグループの追記

--- security.tf
+++ security.tf
@@ -1,45 +1,54 @@
-resource "aws_security_group" "alb_sg" {
+resource "aws_security_group" "keis_alb" {
   vpc_id = aws_vpc.keis_vpc.id
+}
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "keis_alb_ingress_http" {
+	aws_security_group.keis_alb.id
+	cidr_ipv4 = "0.0.0.0/0"
+	ip_protocol = "tcp"
+	from_port = 80
+	to_port = 80
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_vpc_security_group_ingress_rule" "keis_alb_ingress_https" {
+        aws_security_group.keis_alb.id
+        cidr_ipv4 = "0.0.0.0/0"
+        ip_protocol = "tcp"
+        from_port = 443
+        to_port = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "keis_alb_egress" {
+	security_group_id = aws_security_group.keis_alb.id
+	cidr_ipv4         = "0.0.0.0/0"
+	ip_protocol       = "-1" # all ports
 }
 # ロードバランサ用
+# ↓EC2用
 
-resource "aws_security_group" "ec2_sg" {
+resource "aws_security_group" "keis_ec2" {
   vpc_id = aws_vpc.keis_vpc.id
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # 本当は自分IPにする
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
-# EC2用
+resource "aws_security_group_ingress_rule" "keis_ec2_http" {
+	security_group_id = aws_security_group.keis_ec2.id
+	ip_protocol = "tcp"
+	from_port = 80
+	to_port = 80
+	referenced_security_group_id = aws_security_group.keis_alb.id
+}
+
+resource "aws_security_group_ingress_rule" "keis_ec2_https" {
+        security_group_id = aws_security_group.keis_ec2.id
+        ip_protocol = "tcp"
+        from_port = 443
+        to_port = 443
+        referenced_security_group_id = aws_security_group.keis_alb.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "keis_ec2_egress" {
+ 	 security_group_id = aws_security_group.keis_ec2.id
+	  cidr_ipv4         = "0.0.0.0/0"
+ 	 ip_protocol       = "-1" # all ports
+}
+
